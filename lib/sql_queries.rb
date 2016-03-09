8b8f3d59281178b5a0d2a07d62d177cc7b716f69
vicfriedman <victoria.m.friedman@gmail.com>
2016-03-09 14:28:26 -0500

    issue #20

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
-def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
+def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) AS amount_left FROM projects JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left >= 0;"
 end
 
