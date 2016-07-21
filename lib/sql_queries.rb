b47fa0773820348c1053d897568f77b9df0ad149
gabe <gj@users.noreply.github.com>
2016-07-21 14:12:41 -0400

    further clarify method requirements

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,11 +6,11 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
 end
 
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "Write your SQL query Here"
 end
 
@@ -18,7 +18,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
 "Write your SQL query Here"
 end
 
-def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
+def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "Write your SQL query Here"
 end
 
@@ -26,6 +26,6 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 "Write your SQL query Here"
 end
 
-def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
+def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "Write your SQL query Here"
 end
